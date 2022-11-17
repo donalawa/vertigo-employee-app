cd0e0c68bc5f869fd2d5b69cd11cfb8966c3ca6b
donalawa <awadonalcien12@gmail.com>
2022-11-17 09:30:48 +0100

    Designed  employee scan code screen

--- app/screens/ScanCodeScreen.js
+++ app/screens/ScanCodeScreen.js
@@ -53,6 +53,7 @@ function ScanCodeScreen({ navigation  }) {
                     onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                     style={StyleSheet.absoluteFillObject}
                     />
+                    
                     </View>
                     <View style={styles.bottomContainer}>
                         <AppButton title={'Tap to Scan Again'} style={styles.button} onPress={() => setScanned(false)} />
